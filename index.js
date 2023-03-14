46ec397a6eab6d5280f0cdaf393579ea6758c5a5
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-03-14 16:45:48 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet( ride){
   // Write your code here!
+  if(ride<=400){
+    return "This one is on me!"
+  }
+  else if (ride> 2000 && ride < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if (ride > 2500){
+    return "No can do."
+  }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city=="NYC"){
+    return"Ok, sounds good."
+  }
+  else{
+    return"No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(myTip){
   // Write your code here!
+  switch(myTip){
+    case 'generous':
+      return "Thank you so much."
+      break;
+    case 'not as generous':
+    return "Thank you."
+    break;
+    default:
+      return "Bye."
+  
+  }
 }
